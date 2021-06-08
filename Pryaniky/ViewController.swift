04e9aef4a15a641d5114e98d35777a92fac3c035
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-06-08 14:59:26 +0300

    Initial Commit

---
+++ Pryaniky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pryaniky
+//
+//  Created by Ibragim Akaev on 08/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
