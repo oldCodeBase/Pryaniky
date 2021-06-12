ac67be25e8993c6b7c4511f43316b0850c2e9e3b
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-06-12 19:01:21 +0300

    Create a TitleLabel

---
+++ Pryaniky/Custom Views/TitleLabel.swift
@@ -0,0 +1,34 @@
+//
+//  TitleLabel.swift
+//  Pryaniky
+//
+//  Created by Ibragim Akaev on 09/06/2021.
+//
+
+import UIKit
+
+final class TitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    convenience init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        self.init(frame: .zero)
+        self.textAlignment  = textAlignment
+        self.font           = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        textColor                 = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor        = 0.9
+        lineBreakMode             = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
