b2038143f9edc84ac1336013f6a81b753810a352
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-06-12 18:52:56 +0300

    Create an AlertContainerView

---
+++ Pryaniky/Custom Views/AlertContainerView.swift
@@ -0,0 +1,28 @@
+//
+//  AlertContainerView.swift
+//  Pryaniky
+//
+//  Created by Ibragim Akaev on 09/06/2021.
+//
+
+import UIKit
+
+final class AlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor       = .systemBackground
+        layer.cornerRadius    = 16
+        layer.borderWidth     = 2
+        layer.borderColor     = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
